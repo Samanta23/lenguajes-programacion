77666601856e3edf39f6d84d9a0d5ed5d765b5ea
T-101 <T-101@PC244>
2016-01-25 19:43:18 -0600

    250116

---
+++ JavaApplication1/src/javaapplication1/Imc.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author T-101
+ */
+/* 
+Clase modelo Imc encapsulada y con dos constructores
+1. Agregar a los atributos modificador de acceso private
+2. por cada atributo implementar un metodo get (getters)
+3. por cada atributo implementar un metodo set (setters)
+**Solo se encapsulan las clases modelo
+*/
+public class Imc {
+    
+     private float peso;
+     private float altura;
+     
+
+    public String calcular(){
+        float imc=peso/(altura*altura);
+        return "Tu imc es:" + imc;
+    }
+     
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+}
